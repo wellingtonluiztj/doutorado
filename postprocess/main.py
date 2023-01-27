62adf6813d8fc9050479d00ea62cd358419930ec
Wellington Luiz <wellingtonluiztj@usp.br>
2023-01-27 17:53:47 -0300

    resolvendo angulo de contato

---
+++ postprocess/main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 27 15:06:45 2023
+
+@author: wsantos
+"""
+
+from angulocontato import Bubble
+
+
+b1 = Bubble(cross, plus)
+
+b1.angcont(cross = 200, plus = 40)
