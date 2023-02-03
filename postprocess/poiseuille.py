4041c759e68cb7b5d0466ad4586784d87481be1f
wellingtonluiztj <wellingtonluiztj@usp.br>
2023-02-03 11:14:17 -0300

    doutorado-pessoal

--- postprocess/poiseuille.py
+++ postprocess/poiseuille.py
@@ -20,22 +20,37 @@ dm = 2133-11
 dt = 308e-9 
 dv = dx/dt
 
-datas = glob.glob("/home/wsantos/Documentos/dados/gnu_output/RES-100.dat")
+time = 5
 
-
+if time >= 0 and time < 10:
+    datas = glob.glob("/home/wsantos/Documentos/dados/gnu_output/RES-00" + str(time) +".dat")
+elif time >= 10 and time < 99:
+    datas = glob.glob("/home/wsantos/Documentos/dados/gnu_output/RES-0" + str(time) + ".dat")
+else:
+    datas = glob.glob("/home/wsantos/Documentos/dados/gnu_output/RES-" + str(time) + ".dat")
+    
+    
 data =  np.loadtxt(datas[0],skiprows=1)
 x,y = data[:,0],data[:,1] 
 velx, vely = data[:,4], data[:,5]
 vel = (velx**2 + vely**2)**(1/2) 
+vel = vel.reshape(int(np.amax(x)), int(np.amax(y)))
 
+listmean = [ ]
+for i in range(len(vel)):
+    avg = np.mean(vel[i])
+    listmean.append(avg)
+velavg = np.array(listmean)
 
+X = list(range(0,75,1))
+X = np.array(X)
 
 fig = plt.figure()
 myimages = []
-cmap = plt.colormaps['inferno']
 
 
-plt.scatter(x*dx, vel*dv, alpha=0.5)
-plt.xlabel(fr'Posição $x(m/s)$', fontdict=font)
-plt.ylabel(fr'Velocity $v(m/s)$', fontdict=font)
+
+plt.plot(X, velavg, 'o', alpha=0.4)
+plt.xlabel(fr'Posição $x(m/s)$')
+plt.ylabel(fr'Velocity $v(m/s)$')
 plt.show()
--- postprocess/vel.py
+++ postprocess/vel.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import glob
 from matplotlib import animation
 from matplotlib import colors
-
+import matplotlib as mpl
 
 font = {'family': 'serif',
         'color':  'white',
@@ -14,42 +14,52 @@ font = {'family': 'serif',
 
 
 #def velgraph(time)
-dt = 308e-9 
-time = 23
+dt = 308e-9
+dx = 617e-7
+dm = 2133-11 
+dv = dx/dt
 
 
-
-if time >=0 and time <10:
-    datas = glob.glob("/home/wsantos/Documentos/dados/gnu_output/RES-00" + str(time) + ".dat")
-elif time >=10 and time <99:
-    datas = glob.glob("/home/wsantos/Documentos/dados/gnu_output/RES-0" + str(time) + ".dat")
-else:
-    datas = glob.glob("/home/wsantos/Documentos/dados/gnu_output/RES-" + str(time) + ".dat")
+time = 100
 
 
-data =  np.loadtxt(datas[0],skiprows=1)
-x,y = data[:,0],data[:,1] 
-velx, vely = data[:,4], data[:,5]
+def velfield(time):
+    if time >=0 and time <10:
+        datas = glob.glob("/home/wsantos/Documentos/dados/gnu_output/RES-00" + str(time) + ".dat")
+    elif time >=10 and time <99:
+        datas = glob.glob("/home/wsantos/Documentos/dados/gnu_output/RES-0" + str(time) + ".dat")
+    else:
+        datas = glob.glob("/home/wsantos/Documentos/dados/gnu_output/RES-" + str(time) + ".dat")
+    
+    
+    data =  np.loadtxt(datas[0],skiprows=1)
+    x,y = data[:,0],data[:,1] 
+    velx, vely = data[:,4], data[:,5]
+    
+    velx = velx.reshape((int(np.amax(x)),int(np.amax(y)))) 
+    vely = vely.reshape((int(np.amax(x)),int(np.amax(y))))
+    x = x.reshape((int(np.amax(x)),int(np.amax(y)))) 
+    y = y.reshape((int(np.amax(x)),int(np.amax(y))))
+    
+    velx = np.transpose(velx)
+    vely = np.transpose(vely)
+    x = np.transpose(x)
+    y = np.transpose(y)
+    vel = (velx**2 + vely**2)**(1/2) 
+    
+    a = np.shape(vel)[0]
+    b = np.shape(vel[1])[0]
+    
+    X, Y = np.meshgrid(np.arange(0,b, 1), np.arange(0,a,1))
+    cmap = plt.colormaps['inferno']
+    M = plt.imshow(vel,cmap = cmap, interpolation='nearest', origin='lower')
+    Q = plt.quiver(X, Y, velx, vely, scale=0.27, width=0.0012)
+    norm = mpl.colors.Normalize(vmin=0, vmax= np.max(vel*dv))
+    return (
+        plt.text( a-16, b-20,  rf'tempo = ${time*dt}\,s$', fontdict=font),
+    plt.axis('off'),
+    plt.colorbar(mpl.cm.ScalarMappable(cmap=cmap, norm = norm),
+                  orientation='vertical')
+)
 
-velx = velx.reshape((int(np.amax(x)),int(np.amax(y)))) 
-vely = vely.reshape((int(np.amax(x)),int(np.amax(y))))
-x = x.reshape((int(np.amax(x)),int(np.amax(y)))) 
-y = y.reshape((int(np.amax(x)),int(np.amax(y))))
-
-velx = np.transpose(velx)
-vely = np.transpose(vely)
-x = np.transpose(x)
-y = np.transpose(y)
-vel = (velx**2 + vely**2)**(1/2) 
-
-a = np.shape(vel)[0]
-b = np.shape(vel[1])[0]
-
-X, Y = np.meshgrid(np.arange(0,b, 1), np.arange(0,a,1))
-cmap = plt.colormaps['inferno']
-plt.text( a-16, b-20,  rf'tempo = ${time*dt}\,s$', fontdict=font)
-M = plt.imshow(vel,cmap = cmap, interpolation='nearest', origin='lower')
-Q = plt.quiver(X, Y, velx, vely, scale=0.37, width=0.00127)
-plt.axis('off')
-plt.colorbar(plt.cm.ScalarMappable(cmap=cmap),
-              orientation='vertical')
+velfield(time = 40)
